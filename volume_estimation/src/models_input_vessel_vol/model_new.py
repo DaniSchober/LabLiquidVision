6ad6c7e9fd903161c3459498602599b6d6f13c5a
DaniSchober <d.schober@tum.de>
2023-05-05 14:57:12 +0200

    Update model_new.py

--- volume_estimation/src/models_input_vessel_vol/model_new.py
+++ volume_estimation/src/models_input_vessel_vol/model_new.py
@@ -11,12 +11,15 @@ class VolumeNet(nn.Module):
         super(VolumeNet, self).__init__()
 
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, padding=2)
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(
             in_channels=32, out_channels=64, kernel_size=5, padding=2
         )
+        self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(
             in_channels=64, out_channels=128, kernel_size=5, padding=2
         )
+        self.bn3 = nn.BatchNorm2d(128)
 
         self.fc1 = nn.Linear(in_features=128 * 4800, out_features=1024)
         self.fc2 = nn.Linear(in_features=1024, out_features=512)
@@ -29,13 +32,13 @@ class VolumeNet(nn.Module):
         x = torch.stack([vessel_depth, liquid_depth, vessel_vol], dim=1) # stack the two input tensors along the channel dimension
         #print(x.shape)
 
-        x = F.relu(self.conv1(x))
+        x = F.relu(self.bn1(self.conv1(x)))
         #x = F.batch_norm(x, momentum=0.1, eps=1e-5)
         x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(self.conv2(x))
+        x = F.relu(self.bn2(self.conv2(x)))
         #x = F.batch_norm(x)
         x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(self.conv3(x))
+        x = F.relu(self.bn3(self.conv3(x)))
         x = F.max_pool2d(x, kernel_size=2)
         x = x.view(-1, 128 * 4800)
         x = F.relu(self.fc1(x))
